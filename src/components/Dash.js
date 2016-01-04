d4222dba0692e0b560a0cbce5e0f15c0193fdc6e
Stevo <sdebaun@sparks.network>
2016-01-03 21:11:46 -0800

    pushpath

--- src/components/Dash.js
+++ src/components/Dash.js
@@ -2,14 +2,16 @@ require('normalize.css');
 // require('styles/App.css');
 
 import React from 'react';
+import { connect } from 'react-redux';
 
-// import RaisedButton from 'material-ui/lib/raised-button'
+import RaisedButton from 'material-ui/lib/raised-button'
 import AppBar from 'material-ui/lib/app-bar'
 
 import ThemeManager from 'material-ui/lib/styles/theme-manager';
 import Theme from '../theme'
 
 import { Link } from 'react-router';
+import { pushPath } from 'redux-simple-router'
 
 class Dash extends React.Component {
   
@@ -18,14 +20,18 @@ class Dash extends React.Component {
       muiTheme: ThemeManager.getMuiTheme(Theme)
     }
   }
+
   render() {
+    const dispatch = this.props.dispatch
+
     return (
       <div className="index">
         <AppBar title='Dash' showMenuIconButton={false}/>
-        <Link to='/'>Go Main</Link>
+        <RaisedButton label='Go Main' onClick={ ()=> dispatch(pushPath('/')) } />
       </div>
     );
   }
+
 }
 
 Dash.defaultProps = {
@@ -35,4 +41,10 @@ Dash.childContextTypes = {
   muiTheme: React.PropTypes.object
 };
 
-export default Dash;
+function mapStateToProps(state) {
+  /* Populated by react-webpack-redux:reducer */
+  const props = { auth: state.auth };
+  return props;
+}
+
+export default connect(mapStateToProps)(Dash);
