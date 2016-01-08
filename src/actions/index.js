bd003168e2d2d5d1a485e832d8916a1ca929c7a8
Stevo <sdebaun@sparks.network>
2016-01-07 22:52:23 -0800

    refactor auth reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,13 +5,48 @@ export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
 export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS'
 export const PROFILE_LOADED = 'PROFILE_LOADED'
 
+function OAuthToProfile(authData) {
+  const provider = authData.provider,
+    d = authData[provider];
+
+  console.log("provider", provider, d);
+
+  switch (provider) {
+    case 'google':
+      return {
+        uid: authData.uid,
+        fullName: d.displayName,
+        email: d.email,
+        profileImageURL: d.profileImageURL
+      }
+    case 'facebook':
+      return {
+        uid: authData.uid,
+        fullName: 'FB Full name',
+        email: 'FB email',
+        profileImageURL: 'FB image url',
+      }
+    default:
+      throw 'Can only handle google or facebook oauth.'
+  }
+}
+
 export function listenToAuth() {
   return function (dispatch, getState) {
     const ref = new Firebase(getState().fbUrl)
     ref.onAuth( (authData)=>{
       if (authData) {
-        // get back a uid, look up or generate the profile, then finally...
-        dispatch(loginSuccess(authData.uid, authData));
+        const profilesRef = ref.child('profile'),
+          authRef = ref.child('auth').child(authData.uid);
+
+        authRef.once('value', (authSnap)=>{
+          const profileKey = authSnap.val()
+          const profileRef = profileKey ? profilesRef.child(profileKey) : profilesRef.push(OAuthToProfile(authData))
+          if (!profileKey) authRef.set(profileRef.name()) // THIS SHOULD BE SET SERVER-SIDE??
+          profileRef.once('value', (profileSnap)=>{
+            dispatch(loginSuccess(authData.uid, profileSnap.val()));
+          })
+        })
       } else {
         dispatch({ type: LOGOUT_SUCCESS });
       }
@@ -25,7 +60,7 @@ export function login() {
     const ref = new Firebase(getState().fbUrl)
     ref.authWithOAuthRedirect('google', (error)=>{
       if (error) { dispatch({type:LOGOUT_SUCCESS}) }
-    })
+    }, {scope:'email'})
   }
 }
 
@@ -41,6 +76,13 @@ export function loginSuccess(uid,profile) {
   return {
     type: LOGIN_SUCCESS,
     uid,
+    profile
+  }
+}
+
+export function profileLoaded(profile) {
+  return {
+    type: PROFILE_LOADED,
     profile
   }
 }
