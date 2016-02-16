faa2ec98e7cb6ae478968feb6cf910e51a7bdbe4
Stevo <sdebaun@sparks.network>
2016-02-16 14:42:50 -0800

    moar

--- src/components/PageLoadSpinner.js
+++ src/components/PageLoadSpinner.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import CircularProgress from 'material-ui/lib/circular-progress'
 
-export default class PageLoadSpinner extends React.Component {
-  render() {
-    return (
-      <div style={{display:'flex',justifyContent:'center',paddingTop:'2em'}}>
-        <CircularProgress mode='indeterminate' size={3}/>
-      </div>
-    );
-  }
-}
+export default ()=>
+  <div style={{display:'flex',justifyContent:'center',paddingTop:'2em'}}>
+    <CircularProgress mode='indeterminate' size={3}/>
+  </div>
--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -4,10 +4,14 @@ import { findMatch } from 'react-flexr'
 
 import NavPopout from 'components/NavPopout'
 
-export default class SideNav extends React.Component {
-  render() {
-    return findMatch('palm') &&
-      <NavPopout>{ this.props.children }</NavPopout> ||
-      <div style={{height:'100%', width:300}}>{this.props.children}</div>
-  }
-}
+export default ({children})=> findMatch('palm') &&
+  <NavPopout>{children}</NavPopout> ||
+  <div style={{height:'100%', width:300}}>{children}</div>
+
+// export default class SideNav extends React.Component {
+//   render() {
+//     return findMatch('palm') &&
+//       <NavPopout>{ this.props.children }</NavPopout> ||
+//       <div style={{height:'100%', width:300}}>{this.props.children}</div>
+//   }
+// }
