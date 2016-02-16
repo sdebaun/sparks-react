f19ded3118021cc4e2ad7100ce8c64a6a2c7dc33
Stevo <sdebaun@sparks.network>
2016-02-16 13:43:16 -0800

    cleanup

--- src/components/styled/Dialog.js
+++ src/components/styled/Dialog.js
@@ -5,22 +5,12 @@ import AppBar from 'material-ui/lib/app-bar'
 import IconButton from 'material-ui/lib/icon-button';
 import Colors from 'material-ui/lib/styles/colors';
 
-// title={title} modal={false} open={open} onRequestClose={this.close}
+const DialogBar = ({title, leftIcon})=>
+  <AppBar title={title} style={{backgroundColor:Colors.amber700}}
+  iconElementLeft={<IconButton disabled={true}>{React.cloneElement(leftIcon,{color:'white'})}</IconButton>}
+  />
 
-class StyledDialog extends React.Component {
-  render() {
-    const {props:{title,leftIcon,open,onRequestClose}} = this
-    const appBar = (
-      <AppBar title={title} style={{backgroundColor:Colors.amber700}}
-      iconElementLeft={<IconButton disabled={true}>{React.cloneElement(leftIcon,{color:'white'})}</IconButton>}
-      />
-    )
-    return (
-      <Dialog modal={false} title={appBar} {...{open,onRequestClose}}>
-        { this.props.children }
-      </Dialog>
-    )
-  }
-}
-
-export default StyledDialog
+export default ({title,leftIcon,open,onRequestClose,children})=>
+  <Dialog title={<DialogBar {...{title,leftIcon}}/>} {...{open,onRequestClose}}>
+    { children }
+  </Dialog>
